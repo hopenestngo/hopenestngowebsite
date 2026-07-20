d3b9afecb99ca113fa539ea39e796211aeb67974
hopenestngo <hopenestcommunity.org2@gmail.com>
2026-07-20 15:02:21 +0300

    Delete style.js

--- style.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.querySelector(".navbar-toggle");
-    const mobileMenu = document.querySelector(".mobile-menu");
-
-    toggleBtn.addEventListener("click", function () {
-        mobileMenu.classList.toggle("show");
-    });
-});
